0694743d74f8925ca86447cc25340b54fbdbe179
Nancy <naina.gupta7815@gmail.com>
2022-03-10 16:51:10 +0530

    Added fn headers

--- math.py
+++ math.py
@@ -1,16 +1,16 @@
-# Add implementation
+# Add implementationsss
 def add(x,y):
     return x+y
 
-# Subtract implementation
+# Subtract implementationsss
 def subtract(x,y):
     return x-y      # on master branch
 
-# Multiply implementation
+# Multiply implementationsss
 def multiply(x,y):
     return x*y    # on Bug456 branch
 
-# Divide implementation
+# Divide implementationsss
 def divide(x,y):
     if y == 0:         # on master branch
         return DIVIDE_BY_0_ERROR
